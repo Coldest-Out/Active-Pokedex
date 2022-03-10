fe791e99ab74b05b066fd9eaf5a3c9905faff458
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2022-03-10 00:16:57 -0800

    Updated scripts.js to include pokemonlist array
    -added first three pokemon objects to pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,32 @@
-alert('Check it out! I\'m learning JavaScript!');
+let pokemonList = [{
+  name: 'Mudkip',
+  //height is in meters
+  height: 0.4,
+  //weight is in kg
+  weight: 7.5,
+  type: ['water'],
+  dexEntryNumber: 258,
+  description: 'To alert it, the fin on its head senses the flow of water. It has the strength to heft boulders.'
+},
 
-let myName = "Brandyn Harrington";
-document.write(myName);
+{
+  name: 'Marshtomp',
+  //height is in meters
+  height: 0.7,
+  //weight is in kg
+  weight: 28,
+  type: ['water', 'ground'],
+  dexEntryNumber: 259,
+  description: 'Its sturdy legs give it sure footing, even in mud. It burrows into dirt to sleep'
+},
 
-let favoriteFood = "Mac N Cheese";
-document.write(favoriteFood);
+{
+  name: 'Swampert',
+  //height is in meters
+  height: 1.5,
+  //weight is in kg
+  weight: 81.9,
+  type: ['water', 'ground'],
+  dexEntryNumber: 300,
+  description: 'It can swim while towing a large ship. It bashes down foes with a swing of its thick arms.'
+}];
