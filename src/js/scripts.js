44911824a6c6aa75d9657e25c3c2b5bba4117939
Coldest-Out <98006784+Coldest-Out@users.noreply.github.com>
2022-04-06 01:15:39 -0700

    added jquery ignore to eslint

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,7 +1,8 @@
+/* eslint-env jquery */
 //IIFE
 let pokemonRepository = (function () {
   let pokemonList = [];
-  let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=151";
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
 
   //Loads the list of 150 pokemon from the apiUrl
   function loadList() {
@@ -50,7 +51,7 @@ let pokemonRepository = (function () {
   //Search pokemon by name
   function findPokemon(searchName) {
     //Clears list upon typing in search bar
-    $(".pokemon-list").empty();
+    $('.pokemon-list').empty();
     //Adds upon searching
     pokemonList.forEach((pokemon) => {
       if (pokemon.name.indexOf(searchName) > -1) {
@@ -70,25 +71,25 @@ let pokemonRepository = (function () {
   //Adding elements to HTML file
   function addListItem(pokemon) {
     //selecting current <ul> in the HTML doc labeled "pokemon-list"
-    let pokemonList = document.querySelector(".pokemon-list");
+    let pokemonList = document.querySelector('.pokemon-list');
     //creates the list
-    let listPokemon = document.createElement("li");
+    let listPokemon = document.createElement('li');
     //Gives list the Bootstrap Class
-    listPokemon.classList.add("group-list-item");
+    listPokemon.classList.add('group-list-item');
     //creates button
-    let button = document.createElement("button");
+    let button = document.createElement('button');
     //makes the button have the pokemons name inside
     button.innerText = pokemon.name;
     //gives button the Bootstrap Class
-    button.classList.add("btn-primary");
-    button.setAttribute("data-toggle", "modal");
-    button.setAttribute("date-target", "#pokemonModal");
+    button.classList.add('btn-primary');
+    button.setAttribute('data-toggle', 'modal');
+    button.setAttribute('date-target', '#pokemonModal');
     //pushes the button to the HTML
     listPokemon.appendChild(button);
     //pushes the list to the HTML
     pokemonList.appendChild(listPokemon);
     //adds an event to the button, records upon click
-    button.addEventListener("click", function (event) {
+    button.addEventListener('click', function () {
       showDetails(pokemon);
     });
   }
@@ -102,43 +103,42 @@ let pokemonRepository = (function () {
 
   //Creating Modal for Pokedex
   function showModal(pokemon) {
-    let modalBody = $(".modal-body");
-    let modalTitle = $(".modal-title");
-    let modalHeader = $(".modal-header");
+    let modalBody = $('.modal-body');
+    let modalTitle = $('.modal-title');
 
     //Empty contents inside
     modalTitle.empty();
     modalBody.empty();
 
     //Creating element for name in modal contents
-    let nameElement = $("<h1>" + pokemon.name + "</h1>");
+    let nameElement = $('<h1>' + pokemon.name + '</h1>');
     //Creating img in the modal
     let imageElementFront = $('<img class="modal-img" style="width:50%">');
-    imageElementFront.attr("src", pokemon.imageUrl);
+    imageElementFront.attr('src', pokemon.imageUrl);
     let imageElementBack = $('<img class="modal-img" style="width:50%">');
-    imageElementBack.attr("src", pokemon.imageUrlBack);
+    imageElementBack.attr('src', pokemon.imageUrlBack);
 
     //Creating element for height in modal
     let heightElement = $(
-      "<p>" + "Height : " + pokemon.height + "-cm" + "</p>"
+      '<p>' + 'Height : ' + pokemon.height + '-cm' + '</p>'
     );
 
     //Creating element for weight in modal
     let weightElement = $(
-      "<p>" + "Weight : " + pokemon.weight + "-kg" + "</p>"
+      '<p>' + 'Weight : ' + pokemon.weight + '-kg' + '</p>'
     );
 
     //Creating element for type in modal
     let typesElement = $(
-      "<p>" +
-        "Types: " +
-        pokemon.types.map((i) => i.type.name).join(", ") +
-        "<p>"
+      '<p>' +
+        'Types: ' +
+        pokemon.types.map((i) => i.type.name).join(', ') +
+        '<p>'
     );
 
     //Creating element for abilities in modal
     let abilitiesElement = $(
-      "<p>" + "Abilities : " + pokemon.abilities.join(", ") + "</p>"
+      '<p>' + 'Abilities : ' + pokemon.abilities.join(', ') + '</p>'
     );
 
     modalTitle.append(nameElement);
